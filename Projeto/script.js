5decf3d98a0dd004c8b556f0591b043132f79a57
G_P_S <gustavo-pereira23@protonmail.com>
2022-05-30 20:20:03 -0300

    draw-half-update

--- Projeto/script.js
+++ Projeto/script.js
@@ -73,7 +73,39 @@ function hideText(){
 }
 Textoo.addEventListener('click', hideText);
 //EDITOR DE TEXTO---------------------------------------
+//DESENHO-----------------------------------------------
+const canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth - 60;
+canvas.height = 350;
 
+let context = canvas.getContext("2d");
+context.fillStyle = "white";
+context.fillRect(0, 0, canvas.width, canvas.height);
+
+let draw_color = "black";
+let draw_width = "2";
+let is_drawing = false;
+
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
+
+function start(event){
+    is_drawing = true;
+    context.beginPath();
+    context.moveTo(event.clientX - canvas.offsetLeft,
+                   event.clientY - canvas.offsetTop);
+    event.preventDefault();
+}
+
+function draw (event){
+    if (!is_drawing){
+        context.lineTo(event.clientX - canvas.offsetLeft,
+                       event.clientY - canvas.offsetTop);
+    }
+}
+//DESENHO-----------------------------------------------
 //MUDAR BACKGROUND--------------------------------------
 function changecolor(getcolo){
     let bg = document.querySelector(".bg");
